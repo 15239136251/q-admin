8ba66002fa462805fbe56933bea1a26b751299d4
qu.hb@sungeon.com <qu.hb@sungeon.com>
2023-04-13 22:24:36 +0800

    ✨ feat(添加一个通知公告页面):

---
+++ src/views/index/brand.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="index content">
+        <h1>brand通知公告</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.content {
+    background-color: #fff;
+    height: 100%;
+    width: 100%;
+}
+</style>
