00d128c589e344a873685320a23c8df43f1378ce
qu.hb@sungeon.com <qu.hb@sungeon.com>
2023-04-13 22:24:17 +0800

    🦄 refactor(路由跳转前将标签缓存到本地):

--- src/router/route.ts
+++ src/router/route.ts
@@ -1,15 +1,41 @@
 import { getStore } from "@/utils/storage"
-import { Router } from "vue-router"
+import { RouteLocationNormalized, Router } from "vue-router"
+import { useTagsStore } from '@/store/tags'
+
+let tagsStore: any = null
+
+
+const interceptor = (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized
+) => {
+    if (tagsStore === null) {
+        tagsStore = useTagsStore()
+    }
+    const { ADD_TAG } = tagsStore
+
+    if (to.path !== '/login' && to.path !== '/404') {
+        const token = getStore({name: 'token'})
+        if (token) {
+            ADD_TAG({
+                label: to.name as string,
+                path: to.path,
+                params: to.params,
+                query: to.query
+            })
+            return true
+        } else {
+            return {
+                path: "/login",
+                replace: true, // 重定向
+              };
+        }
+    }  else {
+        return true
+    }
+}
 
 export default (router: Router) => {
-    router.beforeEach((to, from, next) => {
-        if (to.path !== '/login') {
-            console.log('to', to)
-            const token = getStore({name: 'token'})
-            if (token) next()
-                else next({ path: '/login' })
-        }
-        else next()
-    })
+    router.beforeEach(interceptor)
     return router
 }
