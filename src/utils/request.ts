53c2340b81dbe1a055812992d7c5d363003f46bf
15239136251 <2532281393@qq.com>
2023-06-25 18:01:09 +0800

    ✨ feat(封装axios):

---
+++ src/utils/request.ts
@@ -0,0 +1,78 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import { ElMessage } from 'element-plus'
+
+export class Request {
+    // axios 实例
+    instance: AxiosInstance
+    // 基础配置，url和超时时间
+    baseConfig: AxiosRequestConfig = { baseURL: '/api', timeout: 60000 }
+    constructor(config: AxiosRequestConfig) {
+        this.instance = axios.create(Object.assign(this.baseConfig, config))
+        //返回其他状态码
+        this.instance.defaults.validateStatus = function (status) {
+            return status >= 200 && status <= 500
+        }
+        //跨域请求，允许保存cookie
+        this.instance.defaults.withCredentials = true
+        // NProgress 配置
+        NProgress.configure({
+            showSpinner: false
+        })
+        //http request拦截
+        this.instance.interceptors.request.use(
+            (config) => {
+                //开启 progress bar
+                NProgress.start()
+                // 一般会请求拦截里面加token，用于后端的验证
+                const token = localStorage.getItem("token") as string
+                if(token) {
+                    config.headers!.Authorization = token;
+                }
+                return config;
+            },
+            (error) => {
+                return Promise.reject(error)
+            }
+        )
+
+        //http response 拦截
+        this.instance.interceptors.response.use(
+            (res) => {
+                // 关闭 NProgress
+                NProgress.done()
+                // 获取状态码
+                const status = res.data.code || res.status
+                // 白名单
+                const statusWhiteList: any[] = []
+                const message =
+                    res.data.message ||
+                    res.data.msg ||
+                    res.data.error_description ||
+                    '未知错误'
+                //如果在白名单里则自行catch逻辑处理
+                if (statusWhiteList.includes(status)) return Promise.reject(res)
+                // 如果请求为非100否者默认统一处理
+                if (status !== 100) {
+                    ElMessage.error(message)
+                    return Promise.reject(new Error(message))
+                }
+                return res
+            },
+            (error) => {
+                NProgress.done()
+                return Promise.reject(new Error(error))
+            }
+        )
+    }
+
+    // 定义请求方法
+    public request(config: AxiosRequestConfig): Promise<AxiosResponse> {
+        return this.instance.request(config)
+    }
+
+
+}
+
+export default new Request({})
